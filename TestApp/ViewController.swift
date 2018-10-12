e0eb4457a695fbb9dbb947b53b76dd2bd4015810
Kanat <thebesttrois@gmail.com>
2018-10-12 15:46:19 +0600

    Initial Commit

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by 123 on 12.10.2018.
+//  Copyright © 2018 kanat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
