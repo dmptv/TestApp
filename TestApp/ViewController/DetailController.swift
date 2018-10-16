6ce48a60082cffc4083608d4631d5717e916115e
Kanat <thebesttrois@gmail.com>
2018-10-16 15:59:22 +0600

    picker controller delegate

--- TestApp/ViewController/DetailController.swift
+++ TestApp/ViewController/DetailController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import VK_ios_sdk
 import SDWebImage
+import YangMingShan
+
 
 class DetailController: UIViewController {
     
@@ -22,10 +24,6 @@ class DetailController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
-            addPhotoButton.setTitle("Select Photo", for: .normal)
-        }
-        
         if let user = user {
             profilImageview.sd_setImage(with: user.photo200Url, completed: nil)
             usernameLabel.text = user.firstName + " " + user.lastName
@@ -36,63 +34,49 @@ class DetailController: UIViewController {
     }
 
     @IBAction func addPhoto(_ sender: UIButton) {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.allowsEditing = false
-        
-        if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            picker.sourceType = .camera
-        } else {
-            picker.sourceType = .photoLibrary
-            picker.modalPresentationStyle = .fullScreen
-        }
-        
-        present(picker, animated: true)
+        self.yms_presentAlbumPhotoView(with: self)
     }
+ 
     
 }
 
-extension DetailController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+extension DetailController: YMSPhotoPickerViewControllerDelegate {
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
-            print("Info did not have the required UIImage for the Original Image")
-            dismiss(animated: true)
-            return
+    func photoPickerViewControllerDidReceivePhotoAlbumAccessDenied(_ picker: YMSPhotoPickerViewController!) {
+        let alertController = UIAlertController.init(title: "Allow photo album access?", message: "Need your permission to access photo albumbs", preferredStyle: .alert)
+        let dismissAction = UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil)
+        let settingsAction = UIAlertAction.init(title: "Settings", style: .default) { (action) in
+            UIApplication.shared.open(URL.init(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: { bool in })
         }
+        alertController.addAction(dismissAction)
+        alertController.addAction(settingsAction)
         
-        profilImageview.image = image
-        
-        //        takePictureButton.isHidden = true
-        //        progressView.progress = 0.0
-        //        progressView.isHidden = false
-        //        activityIndicatorView.startAnimating()
-        
-        //        viewModel?.upload(image: image, progressCompletion:
-        //            { [weak self]  percent in
-        //
-        //                self?.progressView.setProgress(percent, animated: true)
-        //
-        //            }, completion:
-        //            { [weak self] tags, colors in
-        //
-        //                self?.takePictureButton.isHidden = false
-        //                self?.progressView.isHidden = true
-        //                self?.activityIndicatorView.stopAnimating()
-        //
-        //                // TODO: - bind data back
-        //                self?.viewModel?.setTags(tags)
-        //                self?.viewModel?.setColors(colors)
-        //
-        //                self?.performSegue(withIdentifier: "ShowResults", sender: self)
-        //        })
-        
-        dismiss(animated: true)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func photoPickerViewControllerDidReceiveCameraAccessDenied(_ picker: YMSPhotoPickerViewController!) {
+        let alertController = UIAlertController.init(title: "Allow camera album access?", message: "Need your permission to take a photo", preferredStyle: .alert)
+        let dismissAction = UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil)
+        let settingsAction = UIAlertAction.init(title: "Settings", style: .default) { (action) in
+            UIApplication.shared.open(URL.init(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: { bool in })
+        }
+        alertController.addAction(dismissAction)
+        alertController.addAction(settingsAction)
+     
+        picker.present(alertController, animated: true, completion: nil)
+    }
+    
+    func photoPickerViewController(_ picker: YMSPhotoPickerViewController!, didFinishPicking image: UIImage!) {
+        picker.dismiss(animated: true) {
+            DispatchQueue.main.async {
+                self.profilImageview.image = image
+            }
+        }
     }
 
+    
 }
-
 
 
 
