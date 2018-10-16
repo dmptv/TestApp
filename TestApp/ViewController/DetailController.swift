ca62600952acd31ca8ac5b170b61ecea9bb3cf66
Kanat <thebesttrois@gmail.com>
2018-10-16 14:03:55 +0600

    image picker delegate - open camera

--- TestApp/ViewController/DetailController.swift
+++ TestApp/ViewController/DetailController.swift
@@ -22,6 +22,10 @@ class DetailController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            addPhotoButton.setTitle("Select Photo", for: .normal)
+        }
+        
         if let user = user {
             profilImageview.sd_setImage(with: user.photo200Url, completed: nil)
             usernameLabel.text = user.firstName + " " + user.lastName
@@ -32,7 +36,87 @@ class DetailController: UIViewController {
     }
 
     @IBAction func addPhoto(_ sender: UIButton) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = false
         
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+            picker.modalPresentationStyle = .fullScreen
+        }
+        
+        present(picker, animated: true)
     }
     
 }
+
+extension DetailController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            print("Info did not have the required UIImage for the Original Image")
+            dismiss(animated: true)
+            return
+        }
+        
+        profilImageview.image = image
+        
+        //        takePictureButton.isHidden = true
+        //        progressView.progress = 0.0
+        //        progressView.isHidden = false
+        //        activityIndicatorView.startAnimating()
+        
+        //        viewModel?.upload(image: image, progressCompletion:
+        //            { [weak self]  percent in
+        //
+        //                self?.progressView.setProgress(percent, animated: true)
+        //
+        //            }, completion:
+        //            { [weak self] tags, colors in
+        //
+        //                self?.takePictureButton.isHidden = false
+        //                self?.progressView.isHidden = true
+        //                self?.activityIndicatorView.stopAnimating()
+        //
+        //                // TODO: - bind data back
+        //                self?.viewModel?.setTags(tags)
+        //                self?.viewModel?.setColors(colors)
+        //
+        //                self?.performSegue(withIdentifier: "ShowResults", sender: self)
+        //        })
+        
+        dismiss(animated: true)
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
