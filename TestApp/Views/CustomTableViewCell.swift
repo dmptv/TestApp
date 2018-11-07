b677911c276931bca35c51943e902a854b739307
Kanat Absadykov <thebesttrois@gmail.com>
2018-11-07 13:10:14 +0600

    cb
    jjj

--- TestApp/Views/CustomTableViewCell.swift
+++ TestApp/Views/CustomTableViewCell.swift
@@ -21,7 +21,7 @@ class CustomTableViewCell: UITableViewCell {
         userImageView.layer.cornerRadius = 40
         userImageView.layer.masksToBounds = true
         
-        ////
+        
 
     }
     
